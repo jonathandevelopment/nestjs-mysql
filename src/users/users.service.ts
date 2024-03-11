7d34b749b8aa1288be43890eafc27570613ed2e6
Jonathancodesurfer <jonathanalpizar43@gmail.com>
2024-03-11 08:42:04 -0600

    added relations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,11 +21,16 @@ export class UsersService {
     }
 
     getUsers() {
-       return this.userRepository.find()
+       return this.userRepository.find({
+        relations: ['tasks', 'profile']
+       })
     }
 
    async getUser(id:number) {
-        const userFound = await this.userRepository.findOne({where:{id}});
+        const userFound = await this.userRepository.findOne({
+            where:{id},
+            relations:['tasks']
+        });
 
         if(!userFound) {
             return new HttpException('User not found', HttpStatus.NOT_FOUND)
